b48ba7a859c6127e1fedd6c597c118be2572cadf
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2024-03-21 13:06:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import time
 
+
 base_url = st.secrets["BASE_URL"]
 
 # Set the title and prevent line breaks
@@ -13,20 +14,6 @@ col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.image("titanic.jpeg", width=int(600 * 0.6))
 
-# Let the user input the username and password
-username = st.text_input('Username')
-password = st.text_input('Password', type='password')
-
-# Get the token
-def get_token(username, password):
-    response = requests.post(f'{base_url}token', data={'username': username, 'password': password})
-    token = response.json()['access_token']
-    return token
-
-# If the user clicks the 'Get Token' button, get the token
-if st.button('Get Token'):
-    API_TOKEN = get_token(username, password)
-    st.write(f'Token: {API_TOKEN}')
 
 # Let the user choose the api endpoint called
 endpoint = st.selectbox("Choose an endpoint", ("RandomForest", "GradientBoosting"))
@@ -37,6 +24,7 @@ if endpoint == "RandomForest":
 else:
     endpoint = "predict2"
 url = base_url + endpoint
+
 
 # Create a form
 with st.form(key="prediction_form"):
@@ -70,12 +58,8 @@ if submit_button:
         "Embarked": Embarked,
     }
 
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-
     try:
-        response = requests.post(url, json=data, headers=headers)
+        response = requests.post(url, json=data)
         response.raise_for_status()  # Raise an exception if the request was unsuccessful
 
         # Use markdown to display the prediction result
